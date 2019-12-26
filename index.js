b5d93bf2597ac2c87017d3e02efef50a6cdfdedd
bahayg <bahayg@gmail.com>
2019-12-25 22:53:05 -0800

    Done.

--- index.js
+++ index.js
@@ -5,15 +5,15 @@ const returnFirstTwoDrivers = function (drivers) {
 
 const returnLastTwoDrivers = function (drivers) {
     return drivers.slice(-2);
-}
+};
 
 const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
 
 function createFareMultiplier(num) {
     return function(fare) {
         return fare*num;
-    }
-}
+    };
+};
 
 const fareDoubler = createFareMultiplier(2);
 
@@ -21,7 +21,7 @@ const fareTripler = createFareMultiplier(3);
 
 const selectDifferentDrivers = function (drivers, funct) {
     return funct(drivers);
-}
+};
 
 // const selectDifferentDrivers = function (drivers, funct) {
 //     if (funct === returnFirstTwoDrivers) {
